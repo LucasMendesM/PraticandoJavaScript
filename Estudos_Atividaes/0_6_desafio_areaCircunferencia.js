f5027ad59816d5139d189e1bd511b5fa91c919c2
LucasMendesM <jpmarquesmendes@gmail.com>
2021-10-31 19:25:50 -0300

    commit 31/10/21

---
+++ Estudos_Atividaes/0_6_desafio_areaCircunferencia.js
@@ -0,0 +1,10 @@
+// PI * raio * raio
+
+let raio = 10;
+const pi = 3.141592;
+let circunferenciaArea = pi * raio * raio;
+
+console.log ("O valor da área é " + circunferenciaArea + "m2.")
+
+// Chamando o valor de PI através do Math.
+console.log (Math.PI);
