301003ffb63418d260b107c2e26a8c55da439bcd
LucasMendesM <jpmarquesmendes@gmail.com>
2021-11-03 16:00:56 -0300

    commit 03/11/21

---
+++ 18_DOM.js
@@ -0,0 +1,4 @@
+//D.O.M : O Modelo de Documento por Objetos é uma convenção multiplataforma e independente de linguagem de programação, fiscalizada pelo entidade World Wide Web Consortium, para representação e interação com objetos em documentos HTML, XHTML e, XML.
+//B.O.M : Traduzido do inglês-O modelo de objeto do navegador é uma convenção específica do navegador que se refere a todos os objetos expostos pelo navegador da web.
+
+//Tutorial w3 sobre D.O.M -> https://www.w3schools.com/js/js_htmldom.asp
