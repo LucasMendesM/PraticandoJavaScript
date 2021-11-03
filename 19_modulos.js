1b2fa940a7953a2d2aab591ea432f13d716a4c54
LucasMendesM <jpmarquesmendes@gmail.com>
2021-11-03 16:35:20 -0300

    commit 03/11/21

---
+++ 19_modulos.js
@@ -0,0 +1,6 @@
+//Modulos em js são funções que podem ser chamadas de qualquer lugar do código.
+//Os módulos permitem que blocos de código como funções, classes e constantes em arquivos separados possam ser importados quando necessário, facilitando o processo de manutenção e reusabilidade de um projeto JavaScript. Antes desse recurso era necessário declarar variáveis globais em arquivos .
+
+// export serve para exportar uma função, classe ou constante para um arquivo separado.
+// import serve para importar uma função, classe ou constante de um arquivo separado.
+
